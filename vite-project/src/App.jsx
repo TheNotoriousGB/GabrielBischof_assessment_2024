3ff057200bd7c05b6b4661c5027390fd13464098
Gabriel <G.Bischof.inf22@stud.bbbaden.ch>
2024-09-06 16:07:41 +0200

    Commentar to start dev server

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,8 @@ import HotelSection from './components/HotelSection';
 import Navbar from './components/Navbar'; 
 import Footer from './components/Footer';
 import './index.css'; 
-
+ //cd vite-project and run npm run dev to start the development server
+ 
 function App() {
   return (
     <div className="App">
